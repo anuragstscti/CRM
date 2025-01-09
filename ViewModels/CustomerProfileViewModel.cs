242852146c763a0bcae48165e08f67f836fc6271
Anurag Singh <anurags@22css.com>
2025-01-09 14:42:36 +0530

    make first name required

--- ViewModels/CustomerProfileViewModel.cs
+++ ViewModels/CustomerProfileViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class CustomerProfileViewModel
     {
-        public string? FirstName { get; set; }
+        public required string FirstName { get; set; }
         public string? MiddleName { get; set; }
         public string? LastName { get; set; }
         public string? FullName { get; set; }
